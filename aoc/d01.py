1c8c8168abae835c6f651c73a2180e9da0aa9cc8
Marius Schmidt <marius@denktmit.de>
2022-06-08 11:09:40 +0200

    Implemented day 01 b

--- aoc/d01.py
+++ aoc/d01.py
@@ -18,3 +18,30 @@ def process_data_a(filepath: str) -> int:
         previous_value = int(line)
     file.close()
     return increases
+
+
+def process_data_b(filepath: str) -> int:
+    file = open(filepath, 'r')
+    lines = file.read().splitlines()
+    slices = sliding_window(lines, 3)
+    previous_value = math.nan
+    increases = 0
+    for my_slice in slices:
+        current_value = int(my_slice[0]) + int(my_slice[1]) + int(my_slice[2])
+        if math.isnan(previous_value):
+            previous_value = current_value
+            continue
+        if current_value > previous_value:
+            increases = increases + 1
+        previous_value = current_value
+    file.close()
+    return increases
+
+
+def sliding_window(elements, window_size):
+    slices = []
+    if len(elements) <= window_size:
+        return elements
+    for i in range(len(elements) - window_size + 1):
+        slices.append(elements[i:i + window_size])
+    return slices
--- aoc/test_d01.py
+++ aoc/test_d01.py
@@ -1,6 +1,7 @@
 import unittest
 
 from aoc.d01 import process_data_a
+from aoc.d01 import process_data_b
 
 
 class TestUtils(unittest.TestCase):
@@ -9,6 +10,12 @@ class TestUtils(unittest.TestCase):
 
     def test_d01a_realdata(self):
         self.assertEqual(1477, process_data_a("d01.realdata.txt"))
+
+    def test_d01b_testdata(self):
+        self.assertEqual(5, process_data_b("d01.testdata.txt"))
+
+    def test_d01b_realdata(self):
+        self.assertEqual(1523, process_data_b("d01.realdata.txt"))
 
 
 if __name__ == '__main__':
